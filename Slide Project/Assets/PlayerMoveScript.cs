0f03b2678b05508a4711e6ad02263c07ada94566
Y Leung <yleung088@gmail.com>
2023-09-16 00:35:03 +0100

    create-project

---
+++ Slide Project/Assets/PlayerMoveScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rigid : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // gets the current position of the mouse
+        Vector3 mousePos = Input.mousePosition;
+
+        
+    }
+}
