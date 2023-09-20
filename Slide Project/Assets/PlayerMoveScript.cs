b3fe1ed4438abe84bf696deed5d2856d493c3016
Y L <124691469+YtLg@users.noreply.github.com>
2023-09-20 20:37:46 +0100

    Revert Fix
    I have realized that the fix isn't the most effective, instead I plan on seeing how close the value is to 0, and if it is closer than a threshold, then I will add a value to it to make it a larger number

--- Slide Project/Assets/PlayerMoveScript.cs
+++ Slide Project/Assets/PlayerMoveScript.cs
@@ -26,24 +26,32 @@ public class Rigid : MonoBehaviour
         if (Input.GetMouseButtonDown(0)){
 
 
-            float xMousePos = (worldMousePos.x + force);
-            float yMousepos = (worldMousePos.y + force);
+            // Find how close a value is to 0, then if it's closer than a theshold e.g. number is closer to 0 than 3 digits, then add 10 to it.
+            // for negatives, you may have to find the absolute value such as if number = negative then number * -1, if newNumber < 3 then newNumber + 10 * -1;
 
-            if (xMousePos > maxForce){
-                newForceMouse = new Vector3(maxForce, worldMousePos.y, 0);
-            }
-            else if(yMousepos > maxForce){
-                newForceMouse = new Vector3(worldMousePos.x, maxForce, 0);
-            }
-            else if (xMousePos > maxForce && yMousepos > maxForce)
-            {
-                newForceMouse = new Vector3(maxForce, maxForce, 0);
-            }
-            else
-            {
-                newForceMouse = worldMousePos;
-            }
-            myRigidBody.AddForce(newForceMouse * -1);
+            // note to self: you probably could have done if vector.x < 10, vector +10 or smth in order to make it so numbers smaller send the player further without altering the direction
         } 
     }
 }
+
+
+//float xMousePos = (worldMousePos.x + force);
+//float yMousepos = (worldMousePos.y + force);
+
+//if (xMousePos > maxForce && yMousepos > maxForce)
+//{
+//    newForceMouse = new Vector3(maxForce, maxForce, 0);
+//}
+//else if (xMousePos > maxForce)
+//{
+//    newForceMouse = new Vector3(maxForce, yMousepos, 0);
+//}
+//else if (yMousepos > maxForce)
+//{
+//    newForceMouse = new Vector3(xMousePos, maxForce, 0);
+//}
+//else
+//{
+//    newForceMouse = worldMousePos;
+//}
+//myRigidBody.AddForce(newForceMouse * -1);
