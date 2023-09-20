afd2c4a102a0c6d2e69fed60e0b8391126ac45bb
Y L <124691469+YtLg@users.noreply.github.com>
2023-09-20 20:21:29 +0100

    Fix Player Movement
    Because the coordinate system that the player uses to move is based on worldPoint, so the closer the player clicks to 0,0 the less far they move, so I had to add a few calculations to change that

--- Slide Project/Assets/PlayerMoveScript.cs
+++ Slide Project/Assets/PlayerMoveScript.cs
@@ -7,7 +7,9 @@ public class Rigid : MonoBehaviour
 
     public Rigidbody2D myRigidBody;
     public Camera myCamera;
-    public int force;
+    public float force;
+    public float maxForce;
+    private Vector3 newForceMouse;
     // Start is called before the first frame update
     void Start(){
         
@@ -16,12 +18,32 @@ public class Rigid : MonoBehaviour
     // Update is called once per frame
     void Update(){
         // gets the current position of the mouse
-        Vector3 mousePos = Input.mousePosition;
-        Vector3 convMousePos = myCamera.ScreenToWorldPoint(mousePos);
-        convMousePos.z = 0f;
+        Vector3 screenMousePos = Input.mousePosition;
+        Vector3 worldMousePos = myCamera.ScreenToWorldPoint(screenMousePos);
+        worldMousePos.z = 0f;
+        Debug.Log(worldMousePos);
 
         if (Input.GetMouseButtonDown(0)){
-            myRigidBody.AddForce(convMousePos * (force   * -1));
+
+
+            float xMousePos = (worldMousePos.x + force);
+            float yMousepos = (worldMousePos.y + force);
+
+            if (xMousePos > maxForce){
+                newForceMouse = new Vector3(maxForce, worldMousePos.y, 0);
+            }
+            else if(yMousepos > maxForce){
+                newForceMouse = new Vector3(worldMousePos.x, maxForce, 0);
+            }
+            else if (xMousePos > maxForce && yMousepos > maxForce)
+            {
+                newForceMouse = new Vector3(maxForce, maxForce, 0);
+            }
+            else
+            {
+                newForceMouse = worldMousePos;
+            }
+            myRigidBody.AddForce(newForceMouse * -1);
         } 
     }
 }
