b192b857731ab723d4c0ea33f6eed428c386fa36
Y L <124691469+YtLg@users.noreply.github.com>
2023-09-20 19:48:18 +0100

    Create Player Movement

---
+++ Slide Project/Assets/OOBScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OOBScript : MonoBehaviour
+{
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerPos = playerPos = player.transform.position;
+        Debug.Log("Player is currently at: " + playerPos);
+
+    }
+
+        
+
+}
--- Slide Project/Assets/PlayerMoveScript.cs
+++ Slide Project/Assets/PlayerMoveScript.cs
@@ -4,18 +4,24 @@ using UnityEngine;
 
 public class Rigid : MonoBehaviour
 {
+
+    public Rigidbody2D myRigidBody;
+    public Camera myCamera;
+    public int force;
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start(){
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update(){
         // gets the current position of the mouse
         Vector3 mousePos = Input.mousePosition;
+        Vector3 convMousePos = myCamera.ScreenToWorldPoint(mousePos);
+        convMousePos.z = 0f;
 
-        
+        if (Input.GetMouseButtonDown(0)){
+            myRigidBody.AddForce(convMousePos * (force   * -1));
+        } 
     }
 }
