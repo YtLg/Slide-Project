b9d8457ab4dbc6aa52268478bc82e79391c2c487
Y L <124691469+YtLg@users.noreply.github.com>
2023-09-21 17:44:03 +0100

    Create Basic OOB Script
    made it so if the player goes out of bounds it teleports them to the other side of the screen while still maintaining their velocity

--- Slide Project/Assets/OOBScript.cs
+++ Slide Project/Assets/OOBScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class OOBScript : MonoBehaviour
 {
     public GameObject player;
+    public Camera myCamera;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +17,11 @@ public class OOBScript : MonoBehaviour
     void Update()
     {
         Vector3 playerPos = playerPos = player.transform.position;
+
+        if (System.Math.Abs(player.transform.position.y) > 5 || System.Math.Abs(player.transform.position.x) > 13)
+        {
+            player.transform.position = new Vector3(player.transform.position.x *-1, player.transform.position.y * -1, 0f);
+        }
 
     }
 
